b7ac13f4eeff4a319c0bd9411b0989c18b554af3
Marq Gray <marquis.s.sandifer@gmail.com>
2022-01-13 17:56:07 -0600

    1.0.0.3

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -2,13 +2,13 @@
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>http://marqgray.github.io</homePageLink>
+		<homePageLink>https://marqgray.github.io</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="http://marqgray.github.io/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="http://marqgray.github.io/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://marqgray.github.io/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="http://marqgray.github.io/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="http://marqgray.github.io/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://marqgray.github.io/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://marqgray.github.io/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://marqgray.github.io/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://marqgray.github.io/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://marqgray.github.io/wp-json/" />
 			</apis>
 	</service>
 </rsd>
